2775db2c64f2d49d216572e77c5d741fc8493a3f
jayashree <sjayashree529@gmail.com>
2022-05-11 15:32:26 +0530

    addede

---
+++ Add.java
@@ -0,0 +1,17 @@
+ class Add {
+
+	public static void main(String args[]) {
+		int arr[][] = { { 1, 2, 3 }, { 5, 6, 7 }, { 4, 4, 5 } };
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(arr[i][j]+ " ");
+				
+			}
+			System.out.println();
+			
+		}
+
+	}
+}
+
